04c832fb05182ad1578e2e82c99272afc072a627
D Dranidis <dranidis@gmail.com>
2023-12-16 13:21:18 +0200

    Transients Day 16

--- src/advent/2023/d16/core.clj
+++ src/advent/2023/d16/core.clj
@@ -29,40 +29,51 @@
 
 (def visited-m (atom #{}))
 
-(defn light-beam [pi rows cols start-r start-c energized start-dir-r start-dir-c]
+(defn light-beam [pi rows cols start-r start-c energized visited start-dir-r start-dir-c]
   (loop [r start-r
          c start-c
          energized energized
+         visited visited
          dir-r start-dir-r
          dir-c start-dir-c]
-    (if (and (< -1  r rows) (< -1  c cols) (not (@visited-m [r c dir-r dir-c])))
+    (if (and (< -1  r rows) (< -1  c cols) (not (visited [r c dir-r dir-c])))
       (let [ch (get-in pi [r  c])
-            _ (swap! visited-m conj [r c dir-r dir-c])]
+            ;; _ (swap! visited-m conj [r c dir-r dir-c])
+            ]
         (case ch
-          "." (recur (+ r dir-r) (+ c dir-c) (conj energized [r c]) dir-r dir-c)
+          "." (recur (+ r dir-r) (+ c dir-c) (conj! energized [r c])
+                     (conj! visited [r c dir-r dir-c]) dir-r dir-c)
 
           ("\\" "/") (let [[dir-r dir-c] (flip-directions dir-r dir-c ch)]
-                       (recur (+ r dir-r) (+ c dir-c)  (conj energized [r c]) dir-r dir-c))
+                       (recur (+ r dir-r) (+ c dir-c)  (conj! energized [r c])
+                              (conj! visited [r c dir-r dir-c]) dir-r dir-c))
 
           "|" (if (= dir-r 0)
                 (let [to-dn (light-beam pi rows cols (inc r) c
-                                        (conj energized [r c])
+                                        (conj! energized [r c])
+                                        (conj! visited [r c dir-r dir-c])
                                         1 0)
                       to-up (light-beam pi rows cols (dec r) c
-                                        (conj energized [r c])
+                                        (conj! energized [r c])
+                                        (conj! visited [r c dir-r dir-c])
                                         -1 0)]
-                  (set/union (conj energized [r c]) to-dn to-up))
-                (recur (+ r dir-r) (+ c dir-c)  (conj energized [r c]) dir-r dir-c))
+                  energized)
+                (recur (+ r dir-r) (+ c dir-c)  (conj! energized [r c])
+                       (conj! visited [r c dir-r dir-c]) dir-r dir-c))
 
           "-" (if (= dir-c 0)
                 (let [to-rt (light-beam pi rows cols r (inc c)
-                                        (conj energized [r c])
+                                        (conj! energized [r c])
+                                        (conj! visited [r c dir-r dir-c])
                                         0 1)
                       to-lf (light-beam pi rows cols r (dec c)
-                                        (conj energized [r c])
+                                        (conj! energized [r c])
+                                        (conj! visited [r c dir-r dir-c])
                                         0 -1)]
-                  (set/union (conj energized [r c]) to-rt to-lf))
-                (recur (+ r dir-r) (+ c dir-c)  (conj energized [r c]) dir-r dir-c))
+                  energized)
+                (recur (+ r dir-r) (+ c dir-c)  (conj! energized [r c])
+                       (conj! visited [r c dir-r dir-c])
+                       dir-r dir-c))
           (prn "no math" ch)))
       energized)))
 
@@ -75,7 +86,7 @@
     ;; (prn rows cols visited-m)
     ;; (pp/pprint pi)
 
-    (light-beam pi rows cols r c #{} dir-r dir-c)))
+    (persistent! (light-beam pi rows cols r c (transient #{}) (transient #{}) dir-r dir-c))))
 
 (is (= 51 (count (all input 0 3 1 0))))
 
@@ -114,3 +125,17 @@
 ;; "Elapsed time: 289.800896 msecs"
 ;; Day 16 Part 2 7793
 ;; "Elapsed time: 58860.11444 msecs"
+
+;; With transient set for energized and visited as an atom
+;; Day 16 Part 1 7562
+;; "Elapsed time: 63.977959 msecs"
+;; Day 16 Part 2 7793
+;; "Elapsed time: 11659.556277 msecs"
+
+;; With transient sets for both visited and energized
+;; Day 16 Part 1 6286
+;; "Elapsed time: 38.946779 msecs"
+;; Day 16 Part 2 6805
+;; "Elapsed time: 6533.822645 msecs"
+
+
