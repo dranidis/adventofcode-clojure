9a3ad2791031b6dd6af21fa616cf948da4ad6c5f
D Dranidis <dranidis@gmail.com>
2024-12-02 09:50:15 +0200

    Refactor d1

--- src/advent/2024/d1/core.clj
+++ src/advent/2024/d1/core.clj
@@ -1,28 +1,24 @@
 (ns advent.2024.d1.core
   (:require [clojure.string :as str]))
 
-;; (def example (slurp "src/advent/2024/d1/example.txt"))
+;; (def input (slurp "src/advent/2024/d1/example.txt"))
 (def input (slurp "src/advent/2024/d1/input.txt"))
 
-(defn read-a-line [line]
-  (mapv parse-long (re-seq #"\d+" line)))
-
-(defn parse [input read-a-line]
-  (mapv read-a-line (str/split-lines input)))
+(defn parse [input]
+  (for [line (str/split-lines input)]
+    (mapv parse-long (re-seq #"\d+" line))))
 
 ;; Part 1
 (defn total-distance [input]
-  (let [parsed (parse input read-a-line)]
+  (let [parsed (parse input)]
     (apply +
-           (map (fn [f s]
-                  (abs (- f s)))
+           (map (fn [f s] (abs (- f s)))
                 (sort (mapv first parsed))
                 (sort (mapv second parsed))))))
 
 ;; Part 2
-
 (defn similarity-score [input]
-  (let [parsed (parse input read-a-line)]
+  (let [parsed (parse input)]
     (apply +
            (map (fn [x]
                   (* x
