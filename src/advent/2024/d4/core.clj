8a63fc1dfd12ad7f6ac5dc8583ea50e833c6788a
D Dranidis <dranidis@gmail.com>
2024-12-04 09:17:11 +0200

    2024 Day 4

---
+++ src/advent/2024/d4/core.clj
@@ -0,0 +1,57 @@
+(ns advent.2024.d4.core
+  (:require
+   [clojure.string :as str]))
+
+;; (def input "MMMSXXMASM
+;; MSAMXMSMSA
+;; AMXSXMAAMM
+;; MSAMASMSMX
+;; XMASAMXAMM
+;; XXAMMXXAMA
+;; SMSMSASXSS
+;; SAXAMASAAA
+;; MAMMMXMMMM
+;; MXMXAXMASX")
+(def input (slurp "src/advent/2024/d4/input.txt"))
+
+(defn str->2D
+  "Read a string containing new-lines into a 2 dimensional vector of characters"
+  [input]
+  (vec (for [line (str/split-lines input)]
+         (vec (for [c line]
+                (str c))))))
+;; Part 1
+
+(defn answer-1
+  [input]
+  (let [grid (str->2D input)]
+    (count (for [r (range (count grid))
+                 c (range (count (first grid)))
+                 [dx dy] (map vec [[-1 0] [0 1] [1 0] [0 -1] [1 1] [-1 1] [1 -1] [-1 -1]])
+                 :when (= (get-in grid [r c]) "X")
+                 :when (= (get-in grid [(+ r dx) (+ c dy)]) "M")
+                 :when (= (get-in grid [(+ r (* 2 dx)) (+ c (* 2 dy))]) "A")
+                 :when (= (get-in grid [(+ r (* 3 dx)) (+ c (* 3 dy))]) "S")]
+             [r c dx dy]))))
+
+;; Part 2
+
+(defn answer-2
+  [input]
+  (let [grid (str->2D input)]
+    (count
+     (for [r (range (count grid))
+           c (range (count (first grid)))
+           :when (= (get-in grid [r c]) "A")
+           :let [top-l (get-in grid [(dec r) (dec c)])
+                 bottom-r (get-in grid [(inc r) (inc c)])
+                 bottom-l (get-in grid [(inc r) (dec c)])
+                 top-r (get-in grid [(dec r) (inc c)])]
+           :when (every? #{"M" "S"} [top-l bottom-r top-r bottom-l])
+           :when (and (not= top-l bottom-r)
+                      (not= top-r bottom-l))]
+       [r c]))))
+
+(defn -main [& _]
+  (println "Day 1, Part 1:" (answer-1 input))
+  (println "Day 1, Part 2:" (answer-2 input)))
