e0a58a0e6fe7ae181ac2e68ecf2730180e423a42
D Dranidis <dranidis@gmail.com>
2024-12-08 09:53:20 +0200

    Refactor

--- src/advent/2024/d8/core.clj
+++ src/advent/2024/d8/core.clj
@@ -1,6 +1,6 @@
 (ns advent.2024.d8.core
   (:require
-   [advent.util :refer [str->2D]]))
+   [advent.util :refer [in-grid? str->2D]]))
 
 (comment
   (def input "............
@@ -27,6 +27,7 @@
 (def antenna-set (set (remove #(= % ".") (flatten grid))))
 
 (defn antenna-locations
+  "Find the locations of a given antenna in the grid."
   [antenna]
   (for [r (range rows)
         c (range cols)
@@ -34,30 +35,37 @@
     [r c]))
 
 (defn pair-antinodes
+  "Generate antinodes between two antenna locations."
   [from-times to-times]
   (fn [[r1 c1] [r2 c2]]
     (let [dr (- r2 r1)
           dc (- c2 c1)]
-      (filter (fn [[r c]]
-                (and (>= r 0) (>= c 0) (< r rows) (< c cols)))
-              (apply concat
-                     (for [n (range from-times to-times)]
-                       [[(+ r2 (* n dr)) (+ c2 (* n dc))] [(- r1 (* n dr)) (- c1 (* n dc))]]))))))
+      (for [n (range from-times to-times)
+            antinode [[(+ r2 (* n dr)) (+ c2 (* n dc))]
+                      [(- r1 (* n dr)) (- c1 (* n dc))]]
+            :when ((in-grid? rows cols) antinode)]
+        antinode))))
+
 
 (defn antinodes
+  "Find all antinodes for a given antenna."
   [from-times to-times antenna]
-  (apply concat (for [a1 (antenna-locations antenna)
-                      a2 (antenna-locations antenna)
-                      :when (not= a1 a2)]
-                  ((pair-antinodes from-times to-times) a1 a2))))
+  (for [a1 (antenna-locations antenna)
+        a2 (antenna-locations antenna)
+        :when (not= a1 a2)
+        antinode ((pair-antinodes from-times to-times) a1 a2)]
+    antinode))
 
-(def answer-1 (count (set (apply concat (for [antenna antenna-set]
-                                          (antinodes 1 2 antenna))))))
+(def answer-1 (count (set (for [antenna antenna-set
+                                antinode (antinodes 1 2 antenna)]
+                            antinode))))
 
 ;; the answer assumes the worst case scenario where
 ;; two antennas are at adjacent locations at the edge of the grid
-(def answer-2 (count (set (apply concat (for [antenna antenna-set]
-                                          (antinodes 0 rows antenna))))))
+;; and sets to-times to rows
+(def answer-2 (count (set (for [antenna antenna-set
+                                antinode (antinodes 0 rows antenna)]
+                            antinode))))
 
 (defn -main [& _]
   (println "Day 1, Part 1:" answer-1)
