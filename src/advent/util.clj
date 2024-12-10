b2164e9ac9ae8a20c9fb171df1cd0c983d31e6d9
D Dranidis <dranidis@gmail.com>
2024-12-10 09:14:54 +0200

    Update util.clj

--- src/advent/util.clj
+++ src/advent/util.clj
@@ -50,3 +50,80 @@
               ["7" "8" "9"]])
   ;
   )
+
+(defn in-grid?
+  "Returns a function that checks if a point is INSIDE a grid
+   with the given number of rows and columns"
+  [rows cols]
+  (fn [[r c]]
+    (and (>= r 0) (>= c 0) (< r rows) (< c cols))))
+
+
+(defn draw-grid
+  "Draws a grid to the console.
+   The grid is a 2D array of strings."
+  [grid]
+  (doseq [row grid]
+    (doseq [cell row]
+      (print cell))
+    (println)))
+
+(defn draw-grid-from-points
+  "Draws a grid to the console.
+   The grid is a 2D array of strings.
+   Points is a sequence of [row col] pairs.
+   rows and cols are the dimensions of the grid."
+  [rows cols points]
+  (let [grid (to-array-2d (vec (repeat rows (vec (repeat cols ".")))))]
+    (doseq [[r c] points]
+      (aset grid r c "#"))
+    (draw-grid grid)))
+
+
+(defn middle-value-of-vector [vect]
+  (when-not (empty? vect)
+    (vect (quot (count vect) 2))))
+
+(comment
+  (middle-value-of-vector [1 2 3 4 5])
+  ;; 3
+  ;
+  )
+
+(defn in-vector?
+  "true if coll contains elm"
+  [coll elm]
+  (and (vector? coll)
+       (some #(= elm %) coll)))
+
+;; 
+;; BFS find all paths from start to end
+;; 
+(defn find-all-paths-bfs
+  "Find all paths from start to end using the
+   next-fn for getting the next nodes from the
+   current node. next-fn should return a vector and
+   it could be a function or a map."
+  [next-fn start end]
+  (loop [queue [[start]]
+         paths []]
+    (if (empty? queue)
+      paths
+      (let [path (first queue)
+            current (last path)]
+        (if (= current end)
+          (recur (rest queue) (conj paths path))
+          (let [nexts (next-fn current)
+                new-paths (map #(conj path %) nexts)]
+            (recur (concat new-paths (rest queue)) paths)))))))
+
+(comment
+  (find-all-paths-bfs
+   {:A [:B :C]
+    :B [:C :D]
+    :C [:D :E]
+    :D [:E]
+    :E []}
+   :A :E)
+  ;
+  )
