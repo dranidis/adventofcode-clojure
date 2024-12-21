fee4ab98cb34fac84175da29227ffc0d475291df
D Dranidis <dranidis@gmail.com>
2024-12-21 20:51:52 +0200

    21 Part 1

--- src/advent/2024/d21/core.clj
+++ src/advent/2024/d21/core.clj
@@ -2,12 +2,11 @@
   (:require
    [advent.2023.d17.priority-queue :refer [add-with-priority! extract-min!
                                            make-priority-queue!]]
-   [advent.dijkstra :refer [all-shortest-paths-from-to]]
    [advent.util :refer [coords-of-symbol str->2D str->nums]]
    [clojure.string :as str]
    [clojure.test :refer [is]]))
 
-(def example? true)
+(def example? false)
 (def example "029A
 980A
 179A
@@ -15,48 +14,124 @@
 379A")
 
 (def input (if example? example (slurp "src/advent/2024/d21/input.txt")))
-(defn parse-codes [input]
-  (str/split-lines input))
-(def codes (parse-codes input))
+(def codes (str/split-lines input))
 
 ;; DIJKSTRA
-(defn- shortest-path-from-to
-  [D start to]
-  (println "Shortest path from" start "to" to "D" D)
-  (vec (loop [to to
-              path (list to)]
-         (if (= to start)
-           path
-           (let [to (second (get D to))]
-             (recur to (conj path to)))))))
+;; (defn path-from-to
+;;   [neighbors start end]
+;;   ;; Q is a priority queue of [distance previous-node node]
+;;   ;; D is a map from node to [distance previous-node]
+;;   (let [Q (add-with-priority! (make-priority-queue!
+;;                                (fn [[d _] [d2 _]] (< d d2))) [0 nil start ""])
+;;         D {}]
+;;     (loop [Q Q
+;;            D D]
+;;       (if-not (empty? Q)
+;;         (let [[dist prev node cmdstr] (extract-min! Q)]
+;;           ;; (println "Current node" node "distance" dist "queue" Q "prev" prev)
+;;           ;; (println "Distances" D)
 
-(defn path-from-to
-  [neighbors start end]
-  ;; Q is a priority queue of [distance previous-node node]
-  ;; D is a map from node to [distance previous-node]
+;;           ;; If we have already visited the node, skip it.
+;;           (if (= node end)
+;;             cmdstr;;             ;; (shortest-path-from-to (assoc D node [dist prev]) start end)
+;;             (if (get D node);;               (recur Q D)
+;;               (let [D (assoc D node [dist prev])
+;;                     Q (apply add-with-priority! Q
+;;                              (map (fn [[n cmd]] [(inc dist) node n (str cmdstr cmd)])
+;;                                   (neighbors node)))]
+;;                 (recur Q D)))))
+;;         D))))
+
+;; (defn all-paths-from-to
+;;   [neighbors start end]
+;;   ;; Q is a priority queue of [distance previous-node node]
+;;   ;; D is a map from node to [distance previous-node]
+;;   (let [Q (add-with-priority! (make-priority-queue!
+;;                                (fn [[d _] [d2 _]] (< d d2))) [0 nil start ""])
+;;         D {}]
+;;     (loop [Q Q
+;;            D D]
+;;       (if-not (empty? Q)
+;;         (let [[dist prev node cmdstr] (extract-min! Q)
+;;               ns (neighbors node)
+;;               cmds (map (fn [[n cmd]] (str cmdstr cmd)) ns)]
+;;           (if (get D node)
+;;             (let [pdist (first (get D node))]
+;;               (if (= dist pdist)
+;;                 (recur Q (update D node
+;;                                  (fn [[d preds cs]]
+;;                                    [d (conj preds prev)
+;;                                     (conj cs cmdstr)])))
+
+;;                 (recur Q D)))
+;;             (let [D (assoc D node [dist (if prev #{prev} #{}) cmds])
+;;                   D (assoc D node [dist prev])
+;;                   Q (apply add-with-priority! Q
+;;                            (map (fn [[n cmd]] [(inc dist) node n (str cmdstr cmd)])
+;;                                 (neighbors node)))]
+;;               (recur Q D))))
+;;         (get D end)))))
+
+(defn dijkstra-shortest-distances-pred-set
+  "Returns a map from destination node to [distance {previous-nodes}].
+
+   - start: node
+   - neighbors: node -> [[distance neighbor]]
+   - returns: map{destination-node [distance set{previous-nodes}]}
+
+   "
+  [start neighbors]
+  ;; Q is a priority queue of [distance previous-node current-node]
+  ;; D is a map from node to [distance predecessors and commands]
   (let [Q (add-with-priority! (make-priority-queue!
-                               (fn [[d _] [d2 _]] (< d d2))) [0 nil start ""])
+                               (fn [[d _] [d2 _]] (< d d2))) [0 nil start])
         D {}]
     (loop [Q Q
            D D]
       (if-not (empty? Q)
-        (let [[dist prev node cmdstr] (extract-min! Q)]
-          ;; (println "Current node" node "distance" dist "queue" Q "prev" prev)
-          ;; (println "Distances" D)
+        (let [[dist prev node] (extract-min! Q)
+              [n cmd] node
+             ;; Check if we've already visited the node with this distance
+              current-entry (get D n)
+              current-dist (first current-entry)]
+          (if current-entry
+            (if (= dist current-dist)
+            ;; If the node has already been visited with the same distance,
+              ;; add `prev` to predecessors.
+              (recur Q (update D n (fn [[d preds]]
+                                     [d (conj preds [prev cmd])])))
+              ;; else skip it
+              (recur Q D))
 
-          ;; If we have already visited the node, skip it.
-          (if (= node end)
-            cmdstr
-            ;; (shortest-path-from-to (assoc D node [dist prev]) start end)
-            (if (get D node)
-              (recur Q D)
-              (let [D (assoc D node [dist prev])
-                    Q (apply add-with-priority! Q
-                             (map (fn [[n cmd]] [(inc dist) node n (str cmdstr cmd)])
-                                  (neighbors node)))]
-                (recur Q D)))))
+            ;; If the node is visited with a shorter distance, update distance and predecessors.
+            (let [D (assoc D n [dist [[prev cmd]]])
+                  Q (apply add-with-priority! Q
+                           (map (fn [[d neighbor]]
+                                  ;; (println "At node " node "Adding" n "with distance" d)
+                                  [(+ dist d) node neighbor])
+                                (neighbors node)))]
+              (recur Q D))))
         D))))
-;; 
+
+(defn all-shortest-paths-from-to
+  "Return ALL shortest paths from start to end as a list of vectors.
+   The neighbors is a function mapping a node to its neighbors,
+   which are a list of [distance neighbor] pairs."
+  [neighbors start to]
+  (let [D (dijkstra-shortest-distances-pred-set start neighbors)]
+    (letfn [(bt
+              [node]
+              (if (= node (first start))
+                [""]
+                (for [[[pred c] cmd] (second (get D node))
+                      pre (bt pred)]
+                  (str pre cmd))))]
+      (bt to))))
+
+
+
+;;;;;;;;;;;;;;;;;;
+;; KEYPAD
 
 (def num-keypad
   (str->2D
@@ -65,36 +140,52 @@
     123
     #0A"))
 
-(defn num-keypad-next
-  [[r c]]
-  ;;  <
+;; (defn num-keypad-next
+;;   [[r c]]
+;;   (for [[cmd dr dc] [["^" -1 0] ["v" 1 0] ["<" 0 -1] [">" 0 1]]
+;;         :let [nr (+ r dr)
+;;               nc (+ c dc)
+;;               cell (get-in num-keypad [nr nc])]
+;;         :when (and (some? cell)
+;;                    (not= cell "#"))]
+;;     [[nr nc] cmd]))
+
+(defn num-keypad-next-dist
+  [[[r c] cm]]
   (for [[cmd dr dc] [["^" -1 0] ["v" 1 0] ["<" 0 -1] [">" 0 1]]
         :let [nr (+ r dr)
               nc (+ c dc)
               cell (get-in num-keypad [nr nc])]
         :when (and (some? cell)
                    (not= cell "#"))]
-    [[nr nc] cmd]))
+    [1 [[nr nc] cmd]]))
 
-(defn num-keypad-next-dist
-  [[r c]]
-  ;;  <
-  [1 (num-keypad-next [r c])])
-
-(defn position-of-char-on-num-keypad
+(defn num->pos
   [char]
   (first (coords-of-symbol num-keypad char)))
 
 (def position-on-num-keypad
   (assoc (zipmap (map str (range 10))
-                 (map position-of-char-on-num-keypad (map str (range 10))))
+                 (map num->pos (map str (range 10))))
          "A" [3 2]))
 
-(def num-keypad-start (position-on-num-keypad "A"))
+(def num-keypad-start [(position-on-num-keypad "A") ""])
 
-;; 
-;; 
-;; 
+
+(comment
+  (def d0 ((dijkstra-shortest-distances-pred-set
+            num-keypad-start
+            num-keypad-next-dist) (num->pos "0")))
+
+  (all-shortest-paths-from-to num-keypad-next-dist
+                              num-keypad-start (num->pos "4"))
+  ;
+  ;
+  )
+
+;;
+;;
+;;
 
 (def dir-keypad
   (str->2D
@@ -102,16 +193,26 @@
     <v>"))
 
 
-(defn dir-keypad-next
-  [[r c]]
-  ;;  <
+;; (defn dir-keypad-next
+;;   [[r c]]
+;;   ;;  <
+;;   (for [[cmd dr dc] [["^" -1 0] ["v" 1 0] ["<" 0 -1] [">" 0 1]]
+;;         :let [nr (+ r dr)
+;;               nc (+ c dc)
+;;               cell (get-in dir-keypad [nr nc])]
+;;         :when (and (some? cell)
+;;                    (not= cell "#"))]
+;;     [[nr nc] cmd]))
+
+(defn dir-keypad-next-dist
+  [[[r c] cm]]
   (for [[cmd dr dc] [["^" -1 0] ["v" 1 0] ["<" 0 -1] [">" 0 1]]
         :let [nr (+ r dr)
               nc (+ c dc)
               cell (get-in dir-keypad [nr nc])]
         :when (and (some? cell)
                    (not= cell "#"))]
-    [[nr nc] cmd]))
+    [1 [[nr nc] cmd]]))
 
 (defn position-of-char-on-dir-keypad
   [char]
@@ -122,86 +223,159 @@
                  (map position-of-char-on-dir-keypad ["^" "v" "<" ">"]))
          "A" [0 2]))
 
-(def dir-keypad-start (position-on-dir-keypad "A"))
+;; (def dir-keypad-start (position-on-dir-keypad "A"))
+(def dir-keypad-start [(position-on-dir-keypad "A") ""])
 
-
-
-
-
-(defn get-seq-to-type-a-char-on-num-keypad
-  "Receives a 
-   - character to type
-   - and the starting position on the keypad 
-   and returns 
-   - a string of one of the shorterst sequences to perform on the directional keypad
-   - the final position on the keypad.
-   e.g. (type-on-numeric-keypd 0) returns <A"
-  [to-type start]
-  (str (path-from-to num-keypad-next start (position-of-char-on-num-keypad to-type)) "A"))
+;; (defn get-seq-to-type-a-char-on-num-keypad
+;;   "Receives a
+;;    - character to type
+;;    - and the starting position on the keypad
+;;    and returns
+;;    - a string of one of the shorterst sequences to perform on the directional keypad
+;;    - the final position on the keypad.
+;;    e.g. (type-on-numeric-keypd 0) returns <A"
+;;   [to-type start]
+;;   (str (path-from-to num-keypad-next start (num->pos to-type)) "A"))
 
 (defn get-all-seq-to-type-a-char-on-num-keypad
   [to-type start]
-  (str (all-shortest-paths-from-to num-keypad-next-dist start (position-of-char-on-num-keypad to-type)) "A"))
+  (map (fn [s] (str s "A"))
+       (all-shortest-paths-from-to num-keypad-next-dist start (num->pos to-type))))
 
 
 (comment
-  (get-seq-to-type-a-char-on-num-keypad "0" num-keypad-start)
+  (get-all-seq-to-type-a-char-on-num-keypad "8" num-keypad-start)
 ;
   )
 
+;; (defn num->dir
+;;   [str-to-type start]
+;;   (loop [str-to-type str-to-type
+;;          start start
+;;          seq-cmds ""]
+;;     (if (empty? str-to-type)
+;;       seq-cmds
+;;       (let [to-type (str (first str-to-type))
+;;             seq-cmd (get-seq-to-type-a-char-on-num-keypad to-type start)]
+;;         (recur (rest str-to-type)
+;;                (position-on-num-keypad to-type)
+;;                (str seq-cmds seq-cmd))))))
+
 (defn num->dir
   [str-to-type start]
-  (loop [str-to-type str-to-type
-         start start
-         seq-cmds ""]
-    (if (empty? str-to-type)
-      seq-cmds
-      (let [to-type (str (first str-to-type))
-            seq-cmd (get-seq-to-type-a-char-on-num-keypad to-type start)]
-        (recur (rest str-to-type)
-               (position-on-num-keypad to-type)
-               (str seq-cmds seq-cmd))))))
+  (letfn [(num->dir-rec [str-to-type start seq-cmd]
+            ;; (println "Str to type" str-to-type "Start" start "Seq-cmd" seq-cmd)
+            (if (empty? str-to-type)
+              seq-cmd
+              (let [to-type (str (first str-to-type))
+                    ;; _ (println "To type" to-type "Start" start)
+                    seq-cmds (get-all-seq-to-type-a-char-on-num-keypad
+                              to-type start)]
+                ;; (println "To type" to-type "Seq-cmds" seq-cmds)
+                (let [cls (flatten (mapv (fn [sq]
+                                           (num->dir-rec (subs str-to-type 1)
+                                                         [(position-on-num-keypad to-type) ""] sq))
+                                         seq-cmds))]
+                  ;; (prn "Cls" cls "Seq-cmd" seq-cmd)
+                  ;; (prn "returning " (mapv (fn [s] (str seq-cmd s)) cls))
+                  (mapv (fn [s] (str seq-cmd s)) cls)))))]
+    (num->dir-rec str-to-type start "")))
 
+(is (= #{"<A^A>^^AvvvA" "<A^A^>^AvvvA" "<A^A^^>AvvvA"}
+       (set (num->dir "029A" num-keypad-start))))
 (comment
+  (empty? "")
   (def str-to-type "029A")
 
+  (num->dir "A" num-keypad-start)
+  (num->dir "0A" num-keypad-start)
+  (num->dir "02A" num-keypad-start)
   (num->dir "029A" num-keypad-start)
+  (all-shortest-paths-from-to num-keypad-next-dist [3 1] (num->pos "2"))
+  (all-shortest-paths-from-to num-keypad-next-dist [3 2] (num->pos "0"))
+  (get-all-seq-to-type-a-char-on-num-keypad "0" [[3 2] ""])
   ;
   )
 
-(is (some? (#{"<A^A>^^AvvvA" "<A^A^>^AvvvA" "<A^A^^>AvvvA"}
-            (num->dir "029A" num-keypad-start))))
-
-
+;; (defn dir->c->dir
+;;   "Receives a
+;;    - character to type
+;;    - and the starting position on the keypad
+;;    and returns
+;;    - a string of one of the shorterst sequences to perform on the directional keypad
+;;    - the final position on the keypad.
+;;    e.g. (type-on-numeric-keypd 0) returns <A"
+;;   [to-type start]
+;;   (str (path-from-to dir-keypad-next start (position-of-char-on-dir-keypad to-type)) "A"))
 
 (defn dir->c->dir
-  "Receives a 
-   - character to type
-   - and the starting position on the keypad 
-   and returns 
-   - a string of one of the shorterst sequences to perform on the directional keypad
-   - the final position on the keypad.
-   e.g. (type-on-numeric-keypd 0) returns <A"
   [to-type start]
-  (str (path-from-to dir-keypad-next start (position-of-char-on-dir-keypad to-type)) "A"))
+  (map (fn [s] (str s "A"))
+       (all-shortest-paths-from-to dir-keypad-next-dist start (position-of-char-on-dir-keypad to-type))))
+
+(comment
+  (dir->c->dir "<" dir-keypad-start)
+  ;
+  )
+
+;; (defn dir->dir
+;;   [str-to-type start]
+;;   (loop [str-to-type str-to-type
+;;          start start
+;;          seq-cmds ""]
+;;     (if (empty? str-to-type)
+;;       seq-cmds
+;;       (let [to-type (str (first str-to-type))
+;;             seq-cmd (dir->c->dir to-type start)]
+;;         (recur (rest str-to-type)
+;;                (position-on-dir-keypad to-type)
+;;                (str seq-cmds seq-cmd))))))
 
 (defn dir->dir
   [str-to-type start]
-  (loop [str-to-type str-to-type
-         start start
-         seq-cmds ""]
-    (if (empty? str-to-type)
-      seq-cmds
-      (let [to-type (str (first str-to-type))
-            seq-cmd (dir->c->dir to-type start)]
-        (recur (rest str-to-type)
-               (position-on-dir-keypad to-type)
-               (str seq-cmds seq-cmd))))))
+  (letfn [(dir->dir-rec [str-to-type start seq-cmd]
+            ;; (println "Str to type" str-to-type "Start" start "Seq-cmd" seq-cmd)
+            (if (empty? str-to-type)
+              seq-cmd
+              (let [to-type (str (first str-to-type))
+                    ;; _ (println "To type" to-type "Start" start)
+                    seq-cmds (dir->c->dir
+                              to-type start)]
+                ;; (println "To type" to-type "Seq-cmds" seq-cmds)
+                (let [cls (flatten (mapv (fn [sq]
+                                           (dir->dir-rec (subs str-to-type 1)
+                                                         [(position-on-dir-keypad to-type) ""] sq))
+                                         seq-cmds))]
+                  ;; (prn "Cls" cls "Seq-cmd" seq-cmd)
+                  ;; (prn "returning " (mapv (fn [s] (str seq-cmd s)) cls))
+                  (mapv (fn [s] (str seq-cmd s)) cls)))))]
+
+    (dir->dir-rec str-to-type start "")))
 
 (is (= (count "v<<A>>^A<A>AvA<^AA>A<vAAA>^A")
        (count (dir->dir
                (num->dir "029A" num-keypad-start)
                dir-keypad-start))))
+
+(comment
+  (set (map count (flatten (map (fn [w]
+                                  (dir->dir w dir-keypad-start))
+                                (num->dir "029A" num-keypad-start)))))
+
+  (let [cls (flatten (map (fn [w]
+                            (dir->dir w dir-keypad-start))
+                          (num->dir "029A" num-keypad-start)))
+        m (apply min (map count cls))
+        clsf (filter (fn [c] (= m (count c))) cls)
+        csdfd (flatten (map (fn [ww]
+                              (dir->dir ww dir-keypad-start))
+                            clsf))
+        m (apply min (map count csdfd))
+        csdfd (filter (fn [c] (= m (count c))) csdfd)])
+
+  ;
+  )
+
 (comment
   (def str-to-type "029A")
   (def seq-dir (num->dir "029A" num-keypad-start))
@@ -212,43 +386,44 @@
   ;
   )
 
-(defn type-on-num-via-dir
+;; (defn type-on-num-via-dir
+;;   [to-type]
+;;   (let [door-dir (num->dir to-type num-keypad-start)
+;;         dir1 (dir->dir door-dir dir-keypad-start)
+;;         dir2 (dir->dir dir1 dir-keypad-start)]
+;;     (println "Door dir" door-dir)
+;;     (println "Dir 1" dir1)
+;;     (println "Dir 2" dir2)
+;;     dir2))
+
+
+(defn count-type-on-num-via-dir
   [to-type]
-  (let [door-dir (num->dir to-type num-keypad-start)
-        dir1 (dir->dir door-dir dir-keypad-start)
-        dir2 (dir->dir dir1 dir-keypad-start)]
-    (println "Door dir" door-dir)
-    (println "Dir 1" dir1)
-    (println "Dir 2" dir2)
-    dir2))
+  (let [cls (flatten (map (fn [w]
+                            (dir->dir w dir-keypad-start))
+                          (num->dir to-type num-keypad-start)))
+        m (apply min (map count cls))
+        clsf (filter (fn [c] (= m (count c))) cls)
+        csdfd (flatten (map (fn [ww]
+                              (dir->dir ww dir-keypad-start))
+                            clsf))
+        m (apply min (map count csdfd))
+        csdfd (filter (fn [c] (= m (count c))) csdfd)]
+    (count (first csdfd))))
+
 
 (comment
   (def code "029A")
   (str->nums code)
-  (count (type-on-num-via-dir code))
+  (count-type-on-num-via-dir code)
   ;
   )
 
 (defn complexity
   [code]
-  (* (count (type-on-num-via-dir code))
+  (* (count-type-on-num-via-dir code)
      (first (str->nums code))))
 
 (println (apply + (map complexity codes)))
 ;; 296296 too high
-
-(comment
-
-  ;
-  )
-;; 029A: <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
-;; "v<A<AA>>^AvAA^<A>Av<<A>>^AvA^Av<<A>>^Av<A>A^A<Av<A>>^AvA^Av<A<A>>^AAAvA^<A>A"
-
-
-;; (def answer-1 nil)
-;; (def answer-2 nil)
-;; (defn- -main [& _]
-;;   (println "Day XX, Part 1:" answer-1)
-;;   (println "Day XX, Part 2:" answer-2))
-
-;; (-main)
+;; 126384 too low
