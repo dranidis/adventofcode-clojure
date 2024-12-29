13d55c9235a8f252299a37aa9cf4598c12ec4b22
D Dranidis <dranidis@gmail.com>
2024-12-29 16:16:18 +0200

    Use shunting alg (without pars)

--- src/advent/2020/d18/core.clj
+++ src/advent/2020/d18/core.clj
@@ -30,48 +30,7 @@
                       (conj (pop stck) lifo)))))]
           (pars->vec (rest expression) n-expr-with-vectors))))))
 
-(defn evaluate-vec-expression
-  [expression]
-  (letfn
-   [(evl [pe nums op]
-      (if (empty? pe)
-        (if (empty? op)
-          (first nums)
-          (loop [nums nums
-                 op op]
-            (if (empty? op)
-              (first nums)
-              (let [f (first nums)
-                    s (second nums)
-                    r (if (= "+" (first op)) (+ f s) (* f s))]
-                (recur (apply conj [r] (drop 2 nums))
-                       (drop 1 op))))))
-        (let [elem (nth pe 0)]
-          (cond
-            (#{"+" "*"} elem)
-            (evl (rest pe) nums (conj op elem))
 
-            (number? elem)
-            (evl (rest pe) (conj nums elem) op)
-
-            :else ;; collection
-            (let [elem (evl elem [] [])]
-              (evl (rest pe) (conj nums elem) op))))))]
-    (evl expression [] [])))
-
-;; part 1
-(defn compute [expression]
-  (-> expression
-      (pars->vec [])
-      evaluate-vec-expression))
-
-
-(->> input-expressions
-     (map compute)
-     (apply +)
-     println)
-
-;; part 2
 (defn post-fix-calc [post-fix-expr]
   (loop [outp post-fix-expr
          stack []]
@@ -83,13 +42,10 @@
 
           (let [f (peek stack)
                 s (peek (pop stack))
-                res
-                ((read-string sym) f s)
-                ;; (if (= "+" sym) (+ f s) (* f s))
-                ]
+                res (if (= "+" sym) (+ f s) (* f s))]
             (recur (rest outp) (-> stack pop pop (conj res)))))))))
 
-(defn- evaluate-vec-expression-2 [expression]
+(defn- evaluate-vec-expression-2 [precedence expression]
   (letfn
    [(evl [pe nums ops]
       (if (empty? pe)
@@ -102,16 +58,16 @@
 
         (let [elem (nth pe 0)]
           (cond
-            (= elem "+") ;; has higher prec than "*" 
-            (evl (rest pe) nums (conj ops elem))
+            ;; (= elem "+") ;; has higher prec than "*" 
+            ;; (evl (rest pe) nums (conj ops elem))
 
-            (= elem "*") ;; has lower prec than "+"; pop all "+" and push to the out
+            (#{"*" "+"} elem) ;; has lower prec than "+"; pop all "+" and push to the out
             (let [[nums ops] (loop [nums nums
                                     ops ops]
                                (if (empty? ops)
                                  [nums ops]
                                  (let [top (peek ops)]
-                                   (if (= top "+")
+                                   (if (>= (precedence top elem) 0)
                                      (recur (conj nums top) (pop ops))
                                      [nums ops]))))]
               (evl (rest pe) nums (conj ops elem)))
@@ -124,10 +80,29 @@
               (evl (rest pe) (conj nums elem) ops))))))]
     (evl expression [] [])))
 
+(defn compute1 [expression]
+  (-> expression
+      (pars->vec [])
+      ((fn [e] (evaluate-vec-expression-2
+                (fn [a b] 0) ;; use same precedenct for + and * 
+                e)))))
+
+(->> input-expressions
+     (map compute1)
+     (apply +)
+     println)
+
+;; part 2
+
+(defn precedence-plus [sym1 sym2]
+  (cond (= sym1 sym2) 0
+        (= sym1 "+") 1 ;; use higher precedenct for + over *
+        :else -1))
+
 (defn compute2 [expression]
   (-> expression
       (pars->vec [])
-      evaluate-vec-expression-2))
+      ((fn [e] (evaluate-vec-expression-2 precedence-plus e)))))
 
 (->> input-expressions
      (map compute2)
